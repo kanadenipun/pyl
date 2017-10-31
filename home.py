d909a84c24bc5773b9507bf8cf14a618f97b3ae9
NIPUN KANADE <nipunkanade@NIPUN-MacBook-Pro.local>
2017-11-01 02:36:18 +0530

    initial drop

---
+++ home.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+import subprocess, time, socket
+from Adafruit_Thermal import *
+
+
+nextInterval = 0.0   # Time of next recurring operation
+printer      = Adafruit_Thermal("/dev/ttyS0", 19200, timeout=5)
+
+try:
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.connect(('8.8.8.8', 0))
+	printer.print('My IP address is ' + s.getsockname()[0])
+	printer.feed(3)
+except:
+	printer.boldOn()
+	printer.println('Network is unreachable.')
+	printer.boldOff()
+	printer.print('Connect display and keyboard\n'
+	  'for network troubleshooting.')
+	printer.feed(3)
+	exit(0)
+
+
+def interval():
+  p = subprocess.Popen(["python", "pyl.py"],
+    stdout=subprocess.PIPE)
+
+while(True) :
+
+    t = time.time()
+
+    if t > nextInterval:
+        nextInterval = t + 30.0
+        result = interval()
---
+++ pyl.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+from __future__ import print_function
+from Adafruit_Thermal import *
+import requests
+import json
+
+printer      = Adafruit_Thermal("/dev/ttyS0", 19200, timeout=5)
+
+url = "https://api.mlab.com/api/1/databases/krpyl/collections/queue"
+key = "?apiKey="
+
+# insertData = json.loads('{"data":["sdcsd","dsdfsdfsd","fsdfsdf"]}')
+# print("Data Inserted")
+# insert = requests.post(url+key,json=insertData)
+response = requests.get(url+key)
+# print(response.content)
+
+
+if response.status_code == 200 :
+    data = json.loads(response.content)
+    result = data[0]["data"]
+    id = data[0]["_id"]["$oid"]
+
+    for line in result:
+        printer.println(line)
+
+    response = requests.delete(url+"/"+id+key)
+    #print("Deleted")
